ae7fa6fdaf0ea762f712b8ae05b3065ade8d0170
ankireddygudala <ankireddy77299@gmail.com>
2020-01-20 23:50:46 +0530

    state users

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state ={
-      persons :[
-        {name:'Anki Reddy', age:25},
-        {name: 'Rauda ', age: 27},
-        {name: 'virat', age: 30}
-      ],
-      otherState:'Some other state!'
-  }
+const app = props=> {
+ const [personsState, setPersonsState] = useState({
+  persons :[
+    {name:'Anki Reddy', age:25},
+    {name: 'Rauda ', age: 27},
+    {name: 'virat', age: 30}
+  ],
+  otherState:'Some other state!'
+})
 
-  swichNameHandler = ()=>{
-    this.setState({persons :[
-      {name:'Anki', age:25},
-      {name: 'Radra ', age: 27},
-      {name: 'virat', age: 30}
-    ]})
-  }
+const swichNameHandler = ()=>{
+  setPersonsState({persons :[
+    {name:'Anki', age:25},
+    {name: 'Radra ', age: 27},
+    {name: 'virat', age: 30}
+  ]})
+}
 
-  render() {
     return (
       <div className="App">
         <h1> Im react App</h1>
-        <button onClick={this.swichNameHandler}>Swich name</button> 
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} > My hobbies are playing games </Person>
+         <button onClick={swichNameHandler}>Swich name</button> 
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+        <Person na me={personsState.persons[1].name} age={personsState.persons[1].age} />
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age} > My hobbies are playing games </Person>
       </div>
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Welcome to react js!'))
-  }
 }
 
-export default App;
+export default app;
+
+// state ={
+//   persons :[
+//     {name:'Anki Reddy', age:25},
+//     {name: 'Rauda ', age: 27},
+//     {name: 'virat', age: 30}
+//   ],
+//   otherState:'Some other state!'
+// }
+
+// swichNameHandler = ()=>{
+// this.setState({persons :[
+//   {name:'Anki', age:25},
+//   {name: 'Radra ', age: 27},
+//   {name: 'virat', age: 30}
+// ]})
+// }
